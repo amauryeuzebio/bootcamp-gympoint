54a17b1038718e9beb6cc47a2682f626b982b29c
Amaury <amaury@a1tecnologia.info>
2019-10-18 22:20:03 -0300

    Backend: Configuração Eslint e Prettier

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const routes = require('./routes')
-import express from 'express'
-import routes from './routes'
+import express from 'express';
+import routes from './routes';
 
 class App {
-    constructor() {
-        this.server = express()
-        this.middlewares()
-        this.routes()
-    }
-    middlewares() {
-        this.server.use(express.json())
-    }
-    routes() {
-        this.server.use(routes)
-    }
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
 }
 
-export default new App().server 
+export default new App().server;
