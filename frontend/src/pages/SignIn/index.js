94a24b9fd950da15edc91ffd91d5f32bbf3431e7
Amaury <amaury@a1tecnologia.info>
2019-11-06 01:07:09 -0300

    Frontend: --Layout Login --Formularios  (Unform) --Validação (Yup)

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e=mail válido')
+    .required('O e-mail é obrigratorio'),
+  password: Yup.string()
+    .min(6, 'No minimo 6 caracteres')
+    .required('A senha é obrigatoria'),
+});
 
 export default function SignIn() {
-  return <h1>Login</h1>;
+  function handleSubimit({ email, password }) {
+    console.tron.log({ email, password });
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubimit}>
+        <strong>SEU EMAIL</strong>
+        <Input name="email" type="email" placeholder="exemplo@email.com" />
+        <strong>SUA SENHA</strong>
+        <Input name="password" type="password" placeholder="**********" />
+
+        <button type="submit">Entrar no sistema</button>
+      </Form>
+    </>
+  );
 }
--- frontend/src/pages/_layouts/auth/styles.js
+++ frontend/src/pages/_layouts/auth/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
@@ -8,4 +9,64 @@ export const Wrapper = styled.div`
   align-items: center;
 `;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  width: 100%;
+  max-width: 360px;
+  text-align: center;
+  background: #fff;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  padding: 50px 30px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    input {
+      background: #ffffff;
+      border: 1px solid #dddddd;
+      border-radius: 4px;
+      height: 44px;
+      padding: 0 15px;
+      color: ##999999;
+      margin: 0 0 10px;
+
+      &::placeholder {
+        color: #999999;
+      }
+    }
+
+    span {
+      color: #ee4d64;
+      align-self: flex-start;
+      margin: 0 0 10px;
+      font-weight: bold;
+      font-size: 14px;
+    }
+
+    strong {
+      color: #444444;
+      align-self: flex-start;
+      margin: 0 0 10px;
+      font-weight: bold;
+      font-size: 14px;
+    }
+
+    button {
+      margin: 5px 0 0;
+      height: 44px;
+      background: #ee4d64;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#EE4D64')};
+      }
+    }
+  }
+`;
