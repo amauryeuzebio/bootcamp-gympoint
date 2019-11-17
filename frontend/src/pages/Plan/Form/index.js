c9b99330d9bac99d98e72c3d1d884b50c55af0d9
Amaury <amaury@a1tecnologia.info>
2019-11-17 00:38:43 -0300

    Frontend: PlanForm, RegistrationForm

--- frontend/src/pages/Plan/Form/index.js
+++ frontend/src/pages/Plan/Form/index.js
@@ -1,7 +1,118 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import { plansSaveRequest, resetForm } from '~/store/modules/plan/actions';
 
-export default function Form() {
-  return <div />;
+import Button from '~/components/Button';
+import { Grid, Row, Column } from '~/components/Grid';
+
+import {
+  Container,
+  Header,
+  Body,
+  Controls,
+  CustomInput,
+  ReadInput,
+} from './styles';
+
+const schema = Yup.object().shape({
+  title: Yup.string().required(),
+  duration: Yup.number().required(),
+  price: Yup.number().required(),
+});
+
+export default function FormPlan({ match }) {
+  const plan = useSelector(state => state.plan.form);
+  const dispatch = useDispatch();
+  const [duration, setDuration] = useState();
+  const [price, setPrice] = useState(0);
+
+  useEffect(() => {
+    // will Unmount
+    return () => {
+      dispatch(resetForm());
+    };
+    // eslint-disable-next-line
+  }, [])
+
+  useEffect(() => {
+    setDuration(plan.duration);
+    setPrice(plan.price);
+  }, [plan.duration, plan.price]);
+
+  function handleSubmit(data) {
+    if (match.path === '/plan/edit/:id') {
+      dispatch(plansSaveRequest({ id: match.params.id, ...data }));
+    } else {
+      dispatch(plansSaveRequest(data));
+    }
+  }
+
+  return (
+    <Container>
+      <Header>
+        <strong>Cadastro de Plano</strong>
+        <Controls>
+          <Link to="/plan">
+            <Button label="Voltar" color="#CCCCCC" />
+          </Link>
+          <Button label="Salvar" type="submit" form="formPlan" />
+        </Controls>
+      </Header>
+      <Body>
+        <Form
+          initialData={plan}
+          id="formPlan"
+          schema={schema}
+          onSubmit={handleSubmit}
+        >
+          <Grid>
+            <Row>
+              <Column mobile="12" tablet="12" desktop="12">
+                <CustomInput>
+                  <strong>TITULO DO PLANO</strong>
+                  <Input name="title" />
+                </CustomInput>
+              </Column>
+            </Row>
+
+            <Row>
+              <Column mobile="12" tablet="4" desktop="4">
+                <CustomInput>
+                  <strong>DURAÇÃO (em meses)</strong>
+                  <Input
+                    name="duration"
+                    onChange={e => setDuration(e.target.value)}
+                  />
+                </CustomInput>
+              </Column>
+              <Column mobile="12" tablet="4" desktop="4">
+                <CustomInput>
+                  <strong>PREÇO MENSAL</strong>
+                  <Input
+                    name="price"
+                    onChange={e => setPrice(e.target.value)}
+                  />
+                </CustomInput>
+              </Column>
+              <Column mobile="12" tablet="4" desktop="4">
+                <ReadInput>
+                  <strong>PREÇO TOTAL</strong>
+                  <Input name="total" readOnly value={duration * price || 0} />
+                </ReadInput>
+              </Column>
+            </Row>
+          </Grid>
+        </Form>
+      </Body>
+    </Container>
+  );
 }
+
+FormPlan.propTypes = {
+  match: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+};
--- frontend/src/pages/Registration/Form/index.js
+++ frontend/src/pages/Registration/Form/index.js
@@ -1,7 +1,114 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import {
+  registrationsSaveRequest,
+  resetForm,
+} from '~/store/modules/registration/actions';
 
-export default function Form() {
-  return <div />;
+import Button from '~/components/Button';
+import { Grid, Row, Column } from '~/components/Grid';
+
+import {
+  Container,
+  Header,
+  Body,
+  Controls,
+  CustomInput,
+  ReadInput,
+} from './styles';
+
+const schema = Yup.object().shape({
+  student_id: Yup.number().required(),
+  plan_id: Yup.number().required(),
+  start_date: Yup.date().required(),
+});
+
+export default function FormRegistration({ match }) {
+  const registration = useSelector(state => state.registration.form);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // will Unmount
+    return () => {
+      dispatch(resetForm());
+    };
+    // eslint-disable-next-line
+  }, [])
+
+  function handleSubmit(data) {
+    if (match.path === '/registration/edit/:id') {
+      dispatch(registrationsSaveRequest({ id: match.params.id, ...data }));
+    } else {
+      dispatch(registrationsSaveRequest(data));
+    }
+  }
+
+  return (
+    <Container>
+      <Header>
+        <strong>Cadastro de Matricula</strong>
+        <Controls>
+          <Link to="/registration">
+            <Button label="Voltar" color="#CCCCCC" />
+          </Link>
+          <Button label="Salvar" type="submit" form="formRegistration" />
+        </Controls>
+      </Header>
+      <Body>
+        <Form
+          initialData={registration}
+          id="formRegistration"
+          schema={schema}
+          onSubmit={handleSubmit}
+        >
+          <Grid>
+            <Row>
+              <Column mobile="12" tablet="12" desktop="12">
+                <CustomInput>
+                  <strong>ALUNO</strong>
+                  <Input name="studant_id" placeholder="Buscar Aluno" />
+                </CustomInput>
+              </Column>
+            </Row>
+
+            <Row>
+              <Column mobile="12" tablet="3" desktop="3">
+                <CustomInput>
+                  <strong>PLANO</strong>
+                  <Input name="aplan_id" placeholder="Selecione o Plano" />
+                </CustomInput>
+              </Column>
+              <Column mobile="12" tablet="3" desktop="3">
+                <CustomInput>
+                  <strong>DATA INICIO</strong>
+                  <Input name="start_date" placeholder="Escolha a data" />
+                </CustomInput>
+              </Column>
+              <Column mobile="12" tablet="3" desktop="3">
+                <ReadInput>
+                  <strong>DATA TERMINO</strong>
+                  <Input name="end_date" readOnly />
+                </ReadInput>
+              </Column>
+              <Column mobile="12" tablet="3" desktop="3">
+                <ReadInput>
+                  <strong>VALOR FINAL</strong>
+                  <Input name="total" readOnly />
+                </ReadInput>
+              </Column>
+            </Row>
+          </Grid>
+        </Form>
+      </Body>
+    </Container>
+  );
 }
+
+FormRegistration.propTypes = {
+  match: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+};
