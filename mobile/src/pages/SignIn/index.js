0defb34b60bffe5ce3a23e49a39fcb2b8bfd5d19
Amaury <amaury@a1tecnologia.info>
2019-11-28 16:59:40 -0300

    Mobile: Pagina SignIn, COnfiguração Statusbar

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {Image} from 'react-native';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
+
+import * as S from './styles';
 
 export default function SignIn() {
+  const [id, setId] = useState();
+
+  function handleSubmit() {}
+
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <S.Container>
+      <Image source={logo} />
+
+      <S.Form>
+        <S.FormInput
+          placeholder="Informe seu ID de cadastro"
+          keyboardType="number-pad"
+          returnKeyType="send"
+          value={id}
+          onChangeText={setId}
+        />
+        <S.SubmitButton loading={false} onPress={handleSubmit}>
+          Entrar no sistema
+        </S.SubmitButton>
+      </S.Form>
+    </S.Container>
   );
 }
