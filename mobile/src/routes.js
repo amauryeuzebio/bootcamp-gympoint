ec03c89926cea6ac6c9e55323c8d2b9d6e9e4766
Amaury <amaury@a1tecnologia.info>
2019-11-28 20:33:07 -0300

    Mobile: Rota Inicial

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,13 +1,24 @@
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 import SignIn from '~/pages/SignIn';
 import Checkin from '~/pages/Checkin';
-import HelpOrder from '~/pages/HelpOrder';
+import HelpOrder from '~/pages/HelpOrder/List';
 
-export default createAppContainer(
-  createSwitchNavigator({
-    SignIn,
-    Checkin,
-    HelpOrder,
-  })
-);
+export default (signedIn = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+        }),
+        App: createBottomTabNavigator({
+          Checkin,
+          HelpOrder,
+        }),
+      },
+      {
+        initialRouteName: signedIn ? 'App' : 'Sign',
+      }
+    )
+  );
