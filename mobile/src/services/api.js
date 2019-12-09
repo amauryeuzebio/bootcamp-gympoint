e79333d85e66442f85309363001175f17af9bbd9
Amaury Euzebio <amaury@Mac-mini-de-Amaury.local>
2019-12-09 00:59:20 -0300

    Mobile: Path services dinâmicas (IOS e Android)

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,8 +1,10 @@
+import { Platform } from 'react-native';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://10.0.2.2:3333', // Android nao encherga localhost
-  // baseURL: 'http://localhost:3333', // IOS
+  baseURL: Platform.OS === 'ios'
+    ? 'http://localhost:3333'
+    : 'http://10.0.2.2:3333',
 });
 
 export default api;
--- mobile/src/services/socket.js
+++ mobile/src/services/socket.js
@@ -1,6 +1,9 @@
+import { Platform } from 'react-native';
+
 const socket = {
-  baseURL: 'http://10.0.2.2:3333', // Android nao encherga localhost
-  // baseURL: 'http://localhost:3333', // IOS
+  baseURL: Platform.OS === 'ios'
+    ? 'http://localhost:3333'
+    : 'http://10.0.2.2:3333',
 };
 
 export default socket;
