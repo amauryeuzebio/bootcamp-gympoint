f2a1db91c7e1b19568c7d9703724ea171e8db49e
Amaury <amaury@a1tecnologia.info>
2019-12-12 23:38:53 -0300

    Mobile: Fix SplashScreen

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {PersistGate} from 'redux-persist/integration/react';
 import {Provider} from 'react-redux';
-import {StatusBar} from 'react-native';
+import {StatusBar, Platform} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 import './config/ReactotronConfig';
 
@@ -9,6 +10,12 @@ import {store, persistor} from './store';
 import App from './App';
 
 export default function index() {
+  useEffect(() => {
+    if (Platform.OS !== 'ios') {
+      SplashScreen.hide();
+    }
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
